e02b52872fa8762e84805fbb307001611b533629
яна <AvramenkoYana@yandex.ru>
2018-10-14 12:31:20 +0430

    add function 2

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java!");
     }
+    public static void main2(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
