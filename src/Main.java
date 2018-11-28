d8dc6a5bc015141f1514661f8f0cbebe2d60de90
isrpo <44033557+isrpo@users.noreply.github.com>
2018-11-28 22:51:59 +0430

    Update Main.java
    This Close #1, Close #2 and Close isrpo/IDEA#1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hi! How are you?");
+        System.out.println("Hi!!! How are you?");
     }
     public static void main2(String[] args) {
         System.out.println("My first program on Java!");
