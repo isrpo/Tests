cb9e6817c5d18b734204ba8cf7dc8c5558904ea0
isrpo <44033557+isrpo@users.noreply.github.com>
2018-11-28 23:38:36 +0430

    Update Main.java
    Trying to link #2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello. How are you?");
+        System.out.println("Hello. How are you? What are you doing");
     }
     public static void main2(String[] args) {
         System.out.println("My first program on Java!");
