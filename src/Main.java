748c44f9f0cef758563f84d653188fca503965b1
isrpo <44033557+isrpo@users.noreply.github.com>
2018-11-30 16:08:40 +0430

    Update Main.java
    #6

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello. How are you? What are you doing");
+        System.out.println("Hello. How are you?");
     }
     public static void main2(String[] args) {
         System.out.println("My first program on Java!");
